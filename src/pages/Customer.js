f30d800e470f7e8d532ac6bcca2f6a6fbe6978a1
DithyaAnil <adithyaanil002@gmail.com>
2024-09-13 11:07:15 +0530

    Error 404

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,19 +1,28 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import NotFound from "../components/NotFound";
+
 export default function Customer() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [customer, setCustomer] = useState();
+  const [notFound, setNotFound] = useState();
   useEffect(() => {
-    console.log("useEffect");
     const url = "http://localhost:8000/api/customers/" + id;
     fetch(url)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 404) {
+          setNotFound(true);
+        }
+        return response.json();
+      })
       .then((data) => {
         setCustomer(data.customer);
       });
   }, []);
   return (
     <>
+      {notFound ? <NotFound /> : null}
       {customer ? (
         <div>
           <p>{customer.id}</p>
